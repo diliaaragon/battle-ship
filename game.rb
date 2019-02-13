c6fb7e941da9f411bc0faab3cdddf681251e3b09
diliaaragon <dilia.aragon@koombea.com>
2019-02-13 13:48:22 -0500

    Create the attacks_user method for the player to make the attacks to his opponent and an attacks_bot method so that the machine generates an attack randomly, modify the list method to show the array ordered

--- game.rb
+++ game.rb
@@ -1,43 +1,117 @@
 require './board'
 
 class Game
-  attr_accessor :boards_user, :boards_bots
+  attr_accessor :boards_user, :boards_bots, :cont_user, :cont_bot
   
   def initialize
     @boards_users = []
     @boards_bots = []
+    @cont_user = 0
+    @cont_bot = 0
   end
 
   def star
     i = 0
     while i <= 2
       board_user = Board.new
+      (0..6).each do |i|
+        (0..6).each do |j|
+          board_user.matrix[i][j] = 0
+        end
+      end
       @boards_users << board_user
       board_bot = Board.new
+      (0..6).each do |i|
+        (0..6).each do |j|
+          board_bot.matrix[i][j] = 0
+        end
+      end
       @boards_bots << board_bot
       i += 1
     end
   end
 
-  def locate_ship(x,y,orientation)
-
+  def locate_ship_user(x,y,orientation)
     board = @boards_users[0]
     if orientation == "b"
-      (0..3).each do |i|
-        board.matrix[x + i][y] = 0
-      end
+        if x <= 3
+          (0..3).each do |i|
+            board.matrix[x + i][y] = 1
+          end
+        else
+          puts 'En esta pocicion no se puede ubicar'
+        end
     elsif orientation == "a"
-      (0..3).each do |i|
-        board.matrix[x][y + i] = 0
+      if y <= 3
+        (0..3).each do |i|
+          board.matrix[x][y + i] = 1
+       end
+      else
+        puts 'En esta pocicion no se puede ubicar'
       end
     else
       puts 'Incorrect option'
     end
   end
 
-  def list 
-    board = @boards_users[0]
-    puts board.matrix
+  def locate_ship_bot(x,y,orientation)
+    board = @boards_bots[0]
+    if orientation == "b"
+      (0..3).each do |i|
+        board.matrix[x + i][y] = 1
+      end
+    elsif orientation == "a"
+      (0..3).each do |i|
+        board.matrix[x][y + i] = 1
+      end
+    else
+      puts 'Incorrect option'
+    end
   end
 
+  def attacks_user(i, j)
+    board_ship = @boards_bots[0]
+    board_attacks = @boards_users[1]
+    if board_ship.matrix[i][j] == 1
+      board_attacks.matrix[i][j] = 2
+      @cont_user += 1
+      puts 'Le diste'
+    else
+      board_attacks.matrix[i][j] = 3
+      puts 'Agua'
+    end 
+  end
+
+  def attacks_bot(i, j)
+    board_ship = @boards_users[0]
+    board_attacks = @boards_bots[1]
+    if board_ship.matrix[i][j] == 1
+      board_attacks.matrix[i][j] = 2
+      @cont_bot += 1
+      puts 'Le diste'
+    else
+      board_attacks.matrix[i][j] = 3
+      puts 'Agua'
+    end 
+  end
+
+
+  def list 
+    board_ship = @boards_users[0]
+    board_attacks = @boards_users[1]
+   puts 'Ubicacio de su barco'
+    (0..6).each do |i|
+      (0..6).each do |j|
+        print "#{board_ship.matrix[i][j]}  "
+      end
+      print "\n"
+    end
+   puts 'Sus ataques'
+    (0..6).each do |i|
+      (0..6).each do |j|
+        print "#{board_attacks.matrix[i][j]}  "
+      end
+      print "\n"
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 require './game'
 
 game = Game.new
-
 puts """
      
   0 1 2 3 4 5 6 
@@ -23,8 +22,37 @@ puts 'Horientacion'
 puts 'A. Horizontal'
 puts 'B. Vertical'
 orientation = gets.chomp
-game.locate_ship(x,y,orientation)
+game.locate_ship_user(x, y, orientation)
 
-game.list
+print 'Ubicando el barco de su oponente...'
+x = rand(4)
+y = rand(4)
+letters = %w{a b}
+i = letters.length
+orientation = letters[rand(i)]
+game.locate_ship_bot(x, y, orientation)
+print 'Listo'
+puts'------------------------------------------------------------------------------------------------'
 
+while game.cont_user <= 4 or game.cont_bot <= 4
+  puts 'ingrese las cordenas para atacar a su ponente'
+  print 'x: '
+  i = gets.chomp.to_i
+  print 'y: '
+  j = gets.chomp.to_i
+  game.attacks_user(i, j)
+  
+  print 'Su oponente esta armando su estrategia!'
+  i = rand(7)
+  j = rand(7)
+  game.attacks_bot(i, j)
+
+  game.list
+end
+
+if game.cont_user == 4
+  puts 'GANADOR'
+else
+  puts 'HAS PERDIDO'
+end
 
